b0a3f8354be5a69cd98d71a58f2f9928d48c2f97
kiko1134 <krisvas8aelsys@gmail.com>
2021-11-02 20:02:01 +0200

    Added all services

--- diplomaDB/src/main/java/com/example/diplomaDB/repository/ServiceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.diplomaDB.repository;
-
-import com.example.diplomaDB.model.Service;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServiceRepository extends JpaRepository<Service,Long> {
-}
