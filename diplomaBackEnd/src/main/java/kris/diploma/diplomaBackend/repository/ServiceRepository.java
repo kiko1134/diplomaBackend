fa96d2daa6915ebaf6393fcce94340039f7bd085
Kristian Vassikov <krisvas8aelsys@gmail.com>
2022-03-08 22:52:03 +0200

    Delete diplomaBackEnd directory

--- diplomaBackEnd/src/main/java/kris/diploma/diplomaBackend/repository/ServiceRepository.java
+++
@@ -1,7 +0,0 @@
-package kris.diploma.diplomaBackend.repository;
-
-import kris.diploma.diplomaBackend.model.Service;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServiceRepository extends JpaRepository<Service,Long> {
-}
